0b76b19e603144277a5cae33459ebd33f7ce7bcf
Zryan Muhammed Jaafer <zryan.muhammed.jaafer@gmail.com>
2022-09-30 18:18:28 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("hey");
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
 
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
